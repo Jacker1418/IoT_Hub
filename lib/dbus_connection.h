bc7a7d45bd6ed5a5896997861d637c0b7ed625fd
Jacker1418 <jacker1418@gmail.com>
2021-02-08 04:13:40 -1000

    Init

---
+++ lib/dbus_connection.h
@@ -0,0 +1,5 @@
+#ifndef __DBUS_CONNECTION_H__
+#define __DBUS_CONNECTION_H__
+
+
+#endif
---
+++ lib/main.h
@@ -0,0 +1,9 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdio.h>
+
+#include "gdbus.h"
+#include "dbus_connection.h"
+
+#endif
